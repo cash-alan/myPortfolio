ca7127e5292208f6f9b8b3e1af9ed74aceafb1b8
alan-cash <contactalanvalencia@gmail.com>
2023-01-31 23:48:23 +0100

    add the meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import Navbar from 'components/Navbar'
+import Head from 'next/head'
 import 'styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
+        <meta name='author' content='Alan Smith Valencia Izquierdo' />
+        <meta name='description' content='This my portfolio' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
+
       <Navbar />
       <Component {...pageProps} />
     </>
